2ed8809680d66410156246fed7de3a5a5c912605
dikac <rigel234@yahoo.com>
2019-11-23 02:52:23 +0700

    remove breakpoint log

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 'use strict';
 
 
-console.log('1111111111111111111111111111');
 
 const Fs = require('fs-extra');
 
@@ -10,8 +9,6 @@ const root = process.env.INIT_CWD;
 
 let condition = root + '/.gitignore';
 let source = root + '/dist';
-console.log('22222222222222222222222222222222222');
-console.log(condition, source, root);
 
 if(!Fs.pathExistsSync(condition)) {
 
@@ -22,18 +19,4 @@ if(!Fs.pathExistsSync(condition)) {
 
     console.log('package installed successfully');
 
-    // {mkdirp: true}, function(err) {
-    //
-    //     if(err) {
-    //
-    //         console.log(err);
-    //         throw new Error('error when installing package');
-    //
-    //     } else {
-    //
-    //         console.log('package installed successfully');
-    //     }
-    //
-    // });
 }
-console.log('33333333333333333333333333333333');
