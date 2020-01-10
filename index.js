bc94ef98c2edb2247d5705b04eeb343222e65a75
dikac <rigel234@yahoo.com>
2020-01-11 00:45:53 +0700

    remove log

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log(process.env.INIT_CWD);
 console.log(process.env);
 //console.log(process);
 
+const Klaw = require('klaw')
 const Fs = require('fs-extra');
 
 const root = process.env.INIT_CWD;
@@ -19,6 +20,10 @@ if(!Fs.pathExistsSync(condition)) {
 
     Fs.copySync(source, root, { overwrite: true });
 
+
+    for (const file of klaw('/some/dir')) {
+        console.log(file)
+    }
 
    // for (let i = 0; i <= 5; i++) {
 
