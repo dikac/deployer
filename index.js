ba193d89d1560fb6a72e2145722dc95138dd4972
dikac <rigel234@yahoo.com>
2020-01-11 10:20:47 +0700

    add argument log

--- index.js
+++ index.js
@@ -1,70 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 const logUpdate = require('log-update');
-//console.log(process.argv);
+console.log(process.argv);
 //console.log(process.env.INIT_CWD);
 //console.log(process.env);
 //console.log(process);
 
-const klaw = require('klaw');
 const Fs = require('fs-extra');
-const klawSync = require('klaw-sync')
+const klawSync = require('klaw-sync');
 const root = process.env.INIT_CWD;
 
 const condition = root + '/.gitignore';
 const source = '/dist';
 const destination = '/';
 
-class Success  {
-
-    _success = 0;
-    total = 0;
-
-    constructor(total) {
-
-        this.total = total;
-
-    }
-
-    success() {
-        this._success++;
-        logUpdate(`deploying : (${this._success}/${this.total})`);
-    }
-
-    remain() {
-
-        return this.total - this._success;
-    }
-
-};
-
-
-function Move(source, destination, success) {
-
-    let promises = [];
-
-
-
-    return promises;
-}
-
-function Loop(source, destination, success, retry) {
-
-    Promise.all(Move(source, destination, success)).then(()=>{
-
-
-    }).catch((e)=>{
-
-        if(retry === 0) {
-
-            throw e;
-
-        } else {
-
-            return Loop(source, destination, success, --retry);
-        }
-    });
-}
 
 if(!Fs.pathExistsSync(condition)) {
 
@@ -79,6 +28,7 @@ if(!Fs.pathExistsSync(condition)) {
     for (let i = 0; i <= 5; i++) {
 
         error = null;
+
         let files = klawSync(root + source, {nodir: true});
 
         if(total === null) {
@@ -105,6 +55,11 @@ if(!Fs.pathExistsSync(condition)) {
             }
         }
 
+        if(!error) {
+
+            break ;
+        }
+
     }
 
     if(error) {
@@ -115,60 +70,5 @@ if(!Fs.pathExistsSync(condition)) {
 
         logUpdate('deployed');
     }
-
-    //console.log(promises);
-
-
-    //console.log(files);
-
-    // (async ()=> {
-    //
-    //     for await (const file of klaw(root + source, {nodir:true})) {
-    //
-    //         const log = logUpdate.create(process.stdout);
-    //
-    //         const relative = file.path.substr((root + source).length);
-    //         const src = file.path;
-    //         const dest = root + destination + relative;
-    //
-    //         //console.log(src);
-    //         //console.log(dest);
-    //
-    //         log(`installing:${src}`);
-    //
-    //         promises.push(Fs.move(src, dest, { overwrite: true }).then(function () {
-    //
-    //             log.clear();
-    //
-    //         }).catch(e=>{
-    //             log.clear();
-    //             console.log(e);
-    //         }));
-    //
-    //        // console.log(file)
-    //     }
-    // })().then(()=>{
-    //
-    //     console.log(promises);
-    //     Promise.all(promises);
-    // });
-
-
-   // for (let i = 0; i <= 5; i++) {
-
-        // try {
-        //
-        //     Fs.removeSync(source);
-        //   //  break;
-        //
-        // } catch (e) {
-        //
-        //     console.log(`failed retry ${i + 1}`);
-        // }
-  //  }
-
-
-    //console.log(klaw(root + source, {nodir:true}));
-   // console.log('package installed successfully');
 
 }
